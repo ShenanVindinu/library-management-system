e8b83de86e8fce0ac86c04b0c5d5d89f9904e614
ShenanVindinu <shenanvindinu@gmail.com>
2024-03-07 18:33:13 +0530

    dashboard ui created and controller skeleton created css styling applied

---
+++ src/main/java/lk/ijse/controller/DashboardController.java
@@ -0,0 +1,66 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class DashboardController {
+
+    @FXML
+    private TableView<?> bookTable;
+
+    @FXML
+    private TableColumn<?, ?> authorColumn;
+
+    @FXML
+    private TableColumn<?, ?> availabilityColumn;
+
+    @FXML
+    private TableColumn<?, ?> branchColumn;
+
+    @FXML
+    private TableColumn<?, ?> genreColumn;
+
+    @FXML
+    private TableColumn<?, ?> titleColumn;
+
+    @FXML
+    private Button dashboardButton;
+
+    @FXML
+    private Button myProfileButton;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private Button LogoutButton;
+
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visitDashboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visitMyProfile(ActionEvent event) {
+
+    }
+
+}
