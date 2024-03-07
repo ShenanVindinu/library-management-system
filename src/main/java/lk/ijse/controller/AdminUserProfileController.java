66e8ed9f54ca54372620f29a482d1ccee690f04a
ShenanVindinu <shenanvindinu@gmail.com>
2024-03-07 22:11:03 +0530

    admin_user_profile_form.fxml and Controller created

--- src/main/java/lk/ijse/controller/AdminUserProfileController.java
+++ src/main/java/lk/ijse/controller/AdminUserProfileController.java
@@ -1,4 +1,94 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class AdminUserProfileController {
+
+    @FXML
+    private Button LogoutButton;
+
+    @FXML
+    private Button addBookButton;
+
+    @FXML
+    private Button addBranchButton;
+
+    @FXML
+    private TextField authorField;
+
+    @FXML
+    private Button boolManagerButton;
+
+    @FXML
+    private TextField branchField;
+
+    @FXML
+    private Button dashboardButton;
+
+    @FXML
+    private TextField genreField;
+
+    @FXML
+    private Button myProfileButton;
+
+    @FXML
+    private Button removeBookButton;
+
+    @FXML
+    private Button removeBranchButton;
+
+    @FXML
+    private TextField titleField;
+
+    @FXML
+    private Button updateBookButton;
+
+    @FXML
+    void addBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addBranch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeBranch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateBookDetails(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visitBookManager(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visitDashboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visitMyProfile(ActionEvent event) {
+
+    }
+
 }
