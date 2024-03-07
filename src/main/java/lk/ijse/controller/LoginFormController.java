d1ff00776b19ede800bcddeab2b1acb04856fbd5
ShenanVindinu <shenanvindinu@gmail.com>
2024-03-07 16:05:15 +0530

    login page designed and login form and controller skeleton created

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,5 +1,38 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class LoginFormController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField passwordFiled;
+
+    @FXML
+    private Text signupButton;
+
+
+
+
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signup(MouseEvent event) {
+
+    }
 
 }
