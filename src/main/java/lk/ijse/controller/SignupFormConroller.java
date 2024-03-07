69d0d878e8868bc7f8905be5059d096b908f4987
ShenanVindinu <shenanvindinu@gmail.com>
2024-03-07 17:01:13 +0530

    signup_form.fxml page designed and SignupFormConroller skeleton created

---
+++ src/main/java/lk/ijse/controller/SignupFormConroller.java
@@ -0,0 +1,38 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SignupFormConroller {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField passwordFiled;
+
+    @FXML
+    private Button signupButton;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void BackToLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signup(ActionEvent event) {
+
+    }
+
+}
