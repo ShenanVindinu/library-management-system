a2eebd1e7760e1c604cb704da20feca15952f45d
ShenanVindinu <shenanvindinu@gmail.com>
2024-03-08 10:43:38 +0530

    user_details.fxml and UserDetailsController.java Designed and Created

---
+++ src/main/java/lk/ijse/controller/UserDetailsController.java
@@ -0,0 +1,56 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class UserDetailsController {
+
+    @FXML
+    private Button LogoutButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button dashboardButton;
+
+    @FXML
+    private Button myProfileButton;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void backToBookHistory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateUserDetails(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visitDashboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visitMyProfile(ActionEvent event) {
+
+    }
+
+}
