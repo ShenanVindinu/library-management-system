6806cd3f3add9b55bfe87f7ce692a60dc9ac3879
H.S.P.V Perera <132152413+ShenanVindinu@users.noreply.github.com>
2024-03-12 16:48:33 +0530

    User.class updated with constructors, getter and setters and overrided toString method

--- src/main/java/lk/ijse/entity/User.java
+++ src/main/java/lk/ijse/entity/User.java
@@ -14,4 +14,59 @@ public class User {
     private String userName;
 
     private String password;
+
+
+
+    public User() {
+    }
+
+    public User(String name, String email, String userName, String password) {
+        this.name = name;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
 }
