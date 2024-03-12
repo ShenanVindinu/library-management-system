14c81f1ff3108a9388b4b2adf6efab00b30393c1
H.S.P.V Perera <132152413+ShenanVindinu@users.noreply.github.com>
2024-03-12 16:00:52 +0530

    SignupFormConroller can now navigate to load signup_form

--- src/main/java/lk/ijse/controller/SignupFormConroller.java
+++ src/main/java/lk/ijse/controller/SignupFormConroller.java
@@ -2,8 +2,14 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignupFormConroller {
 
@@ -26,8 +32,10 @@ public class SignupFormConroller {
     private TextField username;
 
     @FXML
-    void BackToLogin(ActionEvent event) {
-
+    void BackToLogin(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"));
+        Stage window = (Stage) signupButton.getScene().getWindow();
+        window.setScene(new Scene(rootNode, 800,600));
     }
 
     @FXML
