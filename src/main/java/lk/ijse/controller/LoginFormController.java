94ce4b00595c94f475894b516bc39f2d158651da
H.S.P.V Perera <132152413+ShenanVindinu@users.noreply.github.com>
2024-03-12 15:57:26 +0530

    LoginFormController can now load signup_form

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -2,10 +2,16 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
 
@@ -31,8 +37,10 @@ public class LoginFormController {
     }
 
     @FXML
-    void signup(MouseEvent event) {
-
+    void signup(MouseEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/signup_form.fxml"));
+        Stage window = (Stage) signupButton.getScene().getWindow();
+        window.setScene(new Scene(rootNode, 800,600));
     }
 
 }
